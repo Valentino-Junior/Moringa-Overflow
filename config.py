187add4db4ba45356e23ccd10d3e5efdaf2b386a
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-18 00:01:57 +0300

    Update the drivename and the created db

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'Pitches'
+    SUBJECT_PREFIX = 'Quize'
     SENDER_EMAIL = 'cotechlevy@gmail.com'
     #SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://levy:123456@localhost/pitches'
     
@@ -33,7 +33,7 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://levy:123456@localhost/pitches_tests'
     
 class DevConfig(Config):
-    # SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://levy:123456@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://levy:123456@localhost/mov'
     DEBUG = True
 
 config_options = {
