370a21e45ad67f12cc1adf76671258b097afb47e
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-11-21 19:34:21 +0300

    added styling

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ from app.models import User,Quiz,Comment
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('production')
-# app = create_app('development')
+# app = create_app('production')
+app = create_app('development')
 
 
 manager = Manager(app)
